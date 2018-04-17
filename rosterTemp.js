45293b00472ae5868b1af3f145f1967bf55faed7
sstebner16 <sean.stebner@gmail.com>
2018-04-17 16:27:34 -0400

    Initial commit

---
+++ rosterTemp.js
@@ -0,0 +1,33 @@
+<script>
+    
+    var myRos = {
+"qb1": {
+"firstName" : "Phil",
+"lastName" : "Rivers",
+"team" : "LAC",
+"position" : "QB",
+"salary" : 10,
+"years" : 2,
+"type" : "FA",
+},
+"qb2": {
+"firstName" : "Patrick",
+"lastName":"Mahomes II",
+"team":"KC",
+"position":"QB",
+"salary": 2,
+"years": 3,
+"type":"FA",
+},
+"qb3": {
+"firstName":"Mitchell",
+"lastName":"Trubisky",
+"team":"CHI",
+"position":"QB",
+"salary": 2,
+"years": 3,
+"type":"R",
+}
+}
+
+</script>
